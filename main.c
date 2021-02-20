e27635e3f6634cd262d838b18686c29aea3f8c0d
PradaJoaquin <joaquiprada@gmail.com>
2021-02-20 16:03:44 -0300

    Implementado procesar_entrada y algo de main

--- main.c
+++ main.c
@@ -1,15 +1,61 @@
+#define _POSIX_C_SOURCE 200809L
 #include <stdlib.h>
 #include <stdio.h>
 
+#include "strutil.h"
 #include "mensajes.h"
 
+#define ESCRITURA "W"
+#define LECTURA "R"
 
+void procesar_comando(const char* operacion, const char* direccion, const char* tamanio, const char* datos) {
+	
+}
+
+
+void eliminar_fin_linea(char* linea) {
+	size_t len = strlen(linea);
+	if (linea[len - 1] == '\n') {
+		linea[len - 1] = '\0';
+	}
+}
+
+void procesar_entrada(FILE* archivo_de_trazas) {
+	char* linea = NULL;
+	size_t c = 0;
+	while (getline(&linea, &c, archivo_de_trazas) > 0) {
+		eliminar_fin_linea(linea);
+		char** campos = split(linea, ':');
+		char** parametros = split(campos[1], ' ');
+		procesar_comando(parametros[1], parametros[2], parametros[3], parametros[4]);
+		free_strv(parametros);
+		free_strv(campos);
+	}
+	free(linea);
+}
 
 int main(int argc, char** argv) {
+    // argv[1]: Archivo de traza
+    // argv[2]: Tamaño en bytes
+    // argv[3]: Asociatividad
+    // argv[4]: Numero de sets
+
+    // argv[5]: Modo verboso 
+    // argv[6]: Inicio de rango
+    // argv[7]: Fin de rango
+
 	if(argc != 5 && argc != 8){
 		printf(ERR_NUM_ARG);
 		return 1;
 	}
-	
+
+    FILE* archivo_trazas = fopen(argv[1], "r");
+    if (!archivo_trazas){
+        printf(ERR_ARCH_TRAZAS);
+        return 1;
+    }
+    
+    procesar_entrada(archivo_trazas);
+    fclose(archivo_trazas);
 	return 0;
 }
