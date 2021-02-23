db6561962a0208fd7035c1f578b460c8f5ca80a8
Alexis094 <72041462+Alexis094@users.noreply.github.com>
2021-02-22 21:27:59 -0300

    contiene las estructuras necesarias

---
+++ cache.h
@@ -0,0 +1,41 @@
+#ifndef __CACHE_H__
+#define __CACHE_H__ 
+#include <stdbool.h>
+#include <stdio.h>
+
+#define HIT 1
+#define PENALTY 100 //o 101 con el siguiente HIT. 
+
+typedef struct bloque
+{
+	bool es_valido;  //se activa cuando se realiza una escritura por primera vez en ese set/tag.
+	bool dirty_bit; //se activa cuando se produce un miss y se realiza una escritura.
+	size_t set;    //indica la fila.
+	size_t tag;   //identifica el bloque.
+	size_t instruccion_actual;
+	void* datos; //la unidad minima es de 2 bytes, = void dato[tope];
+}bloque_t;
+
+typedef struct set //fila de bloques, la cantidad de bloques queda definida por el archivo
+{
+	size_t cantidad_sets; 	// parametro "S".
+	bloque_t* sets; 	   // = bloque_t bloques[], cantidad de bloques = "E". 
+}set_t;
+
+typedef struct estadisticas
+{
+	size_t lecturas;	     //se contabilizan siempre.
+	size_t escrituras;      //lecturas + escrituras = N instrucciones.
+	size_t hits;		   //se dan si coinciden (set, tag y validate bit).
+	size_t clean_miss;    //Se activa cuando dirty bit = 0.
+	size_t dirty_rmiss;  //Se contabiliza cuando rmiss + dirty bit = 1.
+	size_t dirty_wmiss; //Se contabiliza cuando wmiss + dirty bit = 1.
+	size_t rmiss;	   //rmiss + wmiss = total miss.
+	size_t wmiss;
+}estadisticas_t;
+
+//bytes written = (escrituras + wmiss) * unidad_datos.
+//bytes read = (lecturas + rmiss) * unidad_datos.
+//miss rate = total miss / (lecturas + escrituras).
+//capacidad cache = S x E x Unidad Datos.
+#endif
