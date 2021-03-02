4570e8b2343d66d6b65fea80777581b38fe1b046
Alexis094 <72041462+Alexis094@users.noreply.github.com>
2021-03-02 13:43:11 -0300

    Estadisticas corregidas

--- estadisticas.c
+++ estadisticas.c
@@ -1,20 +1,5 @@
 #include "estadisticas.h"
 
-size_t penalizacion_rmisses(size_t rmiss){
-	return rmiss * PENALTY;
-}
-
-size_t penalizacion_wmisses(size_t wmiss){
-	return wmiss * PENALTY;
-}
-
-size_t penalizacion_dirty_rmisses(size_t dirty_rmiss){
-	return dirty_rmiss * PENALTY;
-}
-
-size_t penalizacion_dirty_wmisses(size_t dirty_wmiss){
-	return dirty_wmiss * PENALTY;
-}
 
 size_t calcular_readtime(estadisticas_t* estadisticas){
 	return estadisticas->lecturas +  ((estadisticas->rmiss + estadisticas->dirty_rmiss) * PENALTY);
@@ -24,11 +9,39 @@ size_t calcular_writetime(estadisticas_t* estadisticas){
 	return estadisticas->escrituras + ((estadisticas->wmiss + estadisticas->dirty_wmiss) * PENALTY);
 }
 
-//tam = 2048/(128) = 2 bytes por bloque --> 
+/*
+* Tamaño del bloque debe estar validado. 
+*/
 size_t calcular_bytes_written(estadisticas_t* estadisticas, size_t tamanio_bloque){
-	return (estadisticas->escrituras *  tamanio_bloque)/8; //mal
+	return ( (estadisticas->rmiss + estadisticas->wmiss) *  tamanio_bloque); 
 }
 
 size_t calcular_bytes_read(estadisticas_t* estadisticas, size_t tamanio_bloque){
-	return (estadisticas->escrituras *  tamanio_bloque)/8; //mal
+	return ( (estadisticas->dirty_rmiss + estadisticas->dirty_wmiss) *  tamanio_bloque); 
+}
+
+float calcular_miss_rate(estadisticas_t* estadisticas)
+{
+	return (float)(estadisticas->rmiss + estadisticas->wmiss) / 
+	(float)(estadisticas->escrituras + estadisticas->lecturas);
+}
+
+
+/*
+* Recibe el tamanio_cache como cantidad de bytes.
+*/
+void imprimir_estadisticas(estadisticas_t* estadisticas, size_t sets, size_t E, size_t tamanio_cache)
+{
+	if(!estadisticas || sets == 0 || E == 0) return;
+	size_t tamanio_bloque = tamanio_cache/ (sets*E);
+
+	if(E == 1) printf("Direct-mapped, ");
+	else printf("%ld-way, ", E);
+	printf("%ld sets, size = %ldKB \n", sets tamanio_cache/KB);
+	printf("Loads: %ld stores: %ld total: \n", estadisticas->lecturas, estadisticas->escrituras, estadisticas->lecturas + estadisticas->escrituras);
+	printf("rmiss: %ld, wmiss: %ld total: \n", estadisticas->rmiss, estadisticas->wmiss, estadisticas->rmiss + estadisticas->wmiss);
+	printf("dirty rmis: %ld dirty wmiss: %ld \n", estadisticas->dirty_rmiss, estadisticas->dirty_wmiss);
+	printf("bytes read: %ld bytes written: %ld \n", calcular_bytes_read(estadisticas, tamanio_bloque), calcular_bytes_written(estadisticas, tamanio_bloque) );
+	printf("Read time: %ld write time: %ld\n", calcular_readtime(estadisticas), calcular_writetime(estadisticas) );
+	printf("Miss rate: %f \n", calcular_miss_rate(estadisticas) );
 }
--- estadisticas.h
+++ estadisticas.h
@@ -1,7 +1,9 @@
 #ifndef __ESTADISTICAS_H__
 #define __ESTADISTICAS_H__ 
+
 #define HIT 1
-#define PENALTY 100 //o 101 con el siguiente HIT. 
+#define PENALTY 100
+#define KB 1024
 
 #include <stdio.h>
 
@@ -9,9 +11,8 @@ typedef struct cache cache_t;
 
 typedef struct estadisticas
 {
-	size_t lecturas;	     //se contabilizan siempre.
-	size_t escrituras;      //lecturas + escrituras = N instrucciones.
-	size_t hits;		   //se dan si coinciden (set, tag y validate bit).
+	size_t lecturas;	    //se contabilizan siempre.
+	size_t escrituras;     //lecturas + escrituras = N instrucciones.
 	size_t clean_miss;    //Se activa cuando dirty bit = 0.
 	size_t dirty_rmiss;  //Se contabiliza cuando rmiss + dirty bit = 1.
 	size_t dirty_wmiss; //Se contabiliza cuando wmiss + dirty bit = 1.
@@ -19,45 +20,28 @@ typedef struct estadisticas
 	size_t wmiss;
 }estadisticas_t;
 
-//bytes written = ((escrituras + wmiss) * unidad_datos)/8. //mal
+/*
+*  bytes written = ((escrituras + wmiss) * unidad_datos)/8. //mal
+*  bytes read = ((lecturas + rmiss) * unidad_datos)/8. //mal
+*  miss rate = total miss / (lecturas + escrituras).
+*  capacidad cache = S x E x Unidad Datos.
+*  bytes read = total misses x tamanio bloque. (tamanio bloque = cache size /(S*E)).
+*  bytes written = (dirty rmiss + dirty wmiss) * tamanio bloque.
+*/
 
-//bytes read = ((lecturas + rmiss) * unidad_datos)/8. //mal
+/*penalizaciones (ciclos)
+*
+*   clean miss = 1 + penalty; (clean miss = rmiss + wmiss)
+*   dirty miss = 1 + 2*penalty; (dirty miss = d rmiss + s w miss)
+*  	readtime =  (rmiss + dirty_rmiss) * penalty + lecturas;  (1 lectura = 1 ciclo)
+*  	writetime = (wmiss + dirty wmiss) *  penalty + escrituras;
+*/
 
-//miss rate = total miss / (lecturas + escrituras).
-
-//capacidad cache = S x E x Unidad Datos.
-
-//penalizaciones (ciclos)
-//
-//  clean miss = 1 + penalty; (clean miss = rmiss + wmiss)
-//  dirty miss = 1 + 2*penalty; (dirty miss = d rmiss + s w miss)
-//  	readtime =  (rmiss + dirty_rmiss) * penalty + lecturas;  (1 lectura = 1 ciclo)
-//  	writetime = (wmiss + dirty wmiss) *  penalty + escrituras;
-// 		bytes read = (lecturas + rmiss + dirty_rmiss) * tamanio_bloque; //mal
-// 		bytes written = ((escrituras) * tamanio_bloque)/8; 				//mal
-
-size_t penalizacion_rmisses(size_t rmiss);
-
-
-size_t penalizacion_wmisses(size_t wmiss);
-
-
-size_t penalizacion_dirty_rmisses(size_t dirty_rmiss);
-
-
-size_t penalizacion_dirty_wmisses(size_t dirty_wmiss);
-
-
-size_t calcular_readtime(estadisticas_t* estadisticas);
-
-
-size_t calcular_writetime(estadisticas_t* estadisticas);
-
-
-//tam = 2048/(128) = 2 bytes por bloque --> 
-size_t calcular_bytes_written(estadisticas_t* estadisticas, size_t tamanio_bloque);
-
-
-size_t calcular_bytes_read(estadisticas_t* estadisticas, size_t tamanio_bloque);
+/*
+* Recibe el tamanio_cache como cantidad de bytes, los sets y la accesibilidad deben estar validados.
+* Imprime y calcula todas las estadisticas relevantes del el archivo:
+*	lectuas, escrituras, clean misses, dirty misses, bytes leidos y bytes escritos.
+*/
+void imprimir_estadisticas(estadisticas_t* estadisticas, size_t sets, size_t E, size_t tamanio_cache)
 
 #endif //__ESTADISTICAS_H__ 
