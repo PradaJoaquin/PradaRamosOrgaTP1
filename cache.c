e35e9c31ae0004730f98b0286d6e673e87b99ee4
Alexis094 <72041462+Alexis094@users.noreply.github.com>
2021-03-03 14:08:43 -0300

    Agregado encontrar_LRU() ;  bloque_t modificado

--- cache.c
+++ cache.c
@@ -1,5 +1,7 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <stdbool.h>
+
 #include "cache.h"
 
 typedef struct addr
@@ -9,23 +11,24 @@ typedef struct addr
 	int off;
 }addr_t;
 
-typedef struct bloque
-{
-	bool es_valido;  //se activa cuando se realiza una escritura por primera vez en ese set/tag.
-	bool dirty_bit; //se activa cuando se produce un miss y se realiza una escritura.
-	size_t tag;   //identifica el bloque.
-	void* data; //la unidad minima es de 2 bytes, = void dato[tope];
-}bloque_t;
+  typedef struct bloque
+    {
+        bool es_valido;  //se activa cuando se realiza una escritura por primera vez en ese set/tag.
+        bool dirty_bit; //se activa cuando se produce un miss y se realiza una escritura.
+        size_t tag;    //identifica el bloque.
+        void* data;   //la unidad minima es de 2 bytes, = void dato[tope];
+        size_t ins;  //Almacena la instruccion que usa el bloque (la linea del archivo).
+    }bloque_t;
 
 typedef struct set //fila de bloques, la cantidad de bloques queda definida por el archivo
 {
-    size_t E; // Cantidad de bloques = "E"
-	bloque_t* bloques; 	   // = bloque_t bloques[]
+    size_t E;             // Cantidad de bloques = "E"
+	bloque_t* bloques; 	 // = bloque_t bloques[]
 }set_t;
 
 struct cache
 {
-    int time; // Se usa para el last recently used, numero de operacion actual. 
+    int time;  // Se usa para el last recently used, numero de operacion actual. //SE PODRIA SACAR.
     size_t S; // Parametro "S" cantidad de sets
     set_t* sets;
 };
@@ -43,34 +46,123 @@ cache_t* cache_crear(size_t tam, size_t asociatividad, size_t num_sets){
         free(cache);
         return NULL;
     }
+    int k = 0;
     for(int i = 0; i < num_sets; i++){
         cache->sets[i].E = asociatividad;
         cache->sets[i].bloques = malloc(sizeof(bloque_t) * asociatividad);
         if(!cache->sets[i].bloques){
-
-            // Ver como hacer bien para cuando falla esto
-            // Ya que habria que iterar nuevamente para freear los que no fallaron
-
+            for(int j = 0; j < i; j++){
+                for(int q = 0; q < k; q++){
+                    free(cache->sets[j].bloques[q].data);
+                }
+                free(cache->sets[j].bloques);
+            }  
+            free(cache->sets);
+            free(cache);
+            return NULL;
         }
-        for(int k = 0; k < asociatividad; k++){
+        for(k = 0; k < asociatividad; k++){
             cache->sets[i].bloques[k].es_valido = 0;
             cache->sets[i].bloques[k].dirty_bit = 0;
             cache->sets[i].bloques[k].tag = 0;
             cache->sets[i].bloques[k].data = malloc(sizeof(tam_bloque));
             if(!cache->sets[i].bloques[k].data){
-
-                // Lo mismo para aca
-                
+                for(int j = 0; j < i; j++){
+                    for(int q = 0; q < k; q++){
+                        free(cache->sets[j].bloques[q].data);
+                    }
+                    free(cache->sets[j].bloques);
+                }
+                free(cache->sets);
+                free(cache);
+                return NULL;
             }
         }
     }
     return cache;
 }
 
-void* cache_leer(cache_t* cache, size_t direccion){
-
+void cache_destruir(cache_t* cache){
+    for(int i = 0; i < cache->S; i++){
+        for(int k = 0; k < cache->sets[i].E; k++){
+            free(cache->sets[i].bloques[k].data);
+        }
+        free(cache->sets[i].bloques);
+    }
+    free(cache->sets);
+    free(cache);
 }
 
-bool cache_escribir(cache_t* cache, size_t direccion, void* dato){
+op_result_t* cache_operar(cache_t* cache, char* op, size_t dir, size_t tam, size_t datos){
+    op_result_t* result = malloc(sizeof(op_result_t));
+    if(!result){
+        return NULL;
+    }
 
+
+    return result;
+}
+
+/*
+    typedef struct bloque
+    {
+        bool es_valido;  //se activa cuando se realiza una escritura por primera vez en ese set/tag.
+        bool dirty_bit; //se activa cuando se produce un miss y se realiza una escritura.
+        size_t tag;    //identifica el bloque.
+        void* data;   //la unidad minima es de 2 bytes, = void dato[tope];
+        size_t ins;  //Almacena la instruccion que usa el bloque (la linea del archivo).
+    }bloque_t;
+
+    typedef struct set //fila de bloques, la cantidad de bloques queda definida por el archivo
+    {
+        size_t E;             // Cantidad de bloques = "E"
+        bloque_t* bloques;   // = bloque_t bloques[]
+    }set_t;
+
+    struct cache
+    {
+        int time;  // Se usa para el last recently used, numero de operacion actual. //SE PODRIA SACAR.
+        size_t S; // Parametro "S" cantidad de sets
+        set_t* sets;
+    };
+*/
+
+bloque_t* buscar_menores(bloque_t** bloques, size_t tope)
+{
+    size_t ins_menor = *(bloques[0])->ins; //todas son positivas.
+    size_t pos_menor = 0;
+
+    for (int i = 1; i < tope; ++i)
+    {
+        if( *(bloques[i])->ins < ins_menor){
+            //guardo la menor instruccion.
+            ins_menor = *(bloques[i])->ins;
+            //guardo el puntero del bloque en el auxiliar.
+            pos_menor = i;
+        }
+    }
+    return *(bloques[pos_menor]); //puntero al bloque.
+}
+
+
+/*
+*   Dado una matriz cache devuelve el bloque menos usado, siempre devuelve uno. 
+*/
+bloque_t* encontrar_LRU(cache_t* cache)
+{
+    /* Busco los menores por sets, los guardo, y por ultimo devuelvo el menor de los menores. */
+
+    //creo el contenedor auxiliar.
+    bloque_t** menores[cache->S]; //vector de punteros de bloque_t*
+    size_t tope = cache->S;
+    size_t tope_bloques = cache->set->E;
+
+    for (int i = 0; i < tope; ++i)
+    {
+        // voy buscando los menores; cache->sets[]->bloques[].
+        menores[i] = buscar_menores(&(cache->sets[i]->bloques), tope_bloques);
+    }
+
+    //ahora busco el menor de los menores.
+    return buscar_menores(menores, tope);
 }
--- cache.h
+++ cache.h
@@ -8,77 +8,28 @@
 
 typedef struct cache cache_t;
 
-#define HIT 1
-#define CLEAN_MISS 2
-#define DIRTY_MISS 3
+typedef struct estadisticas
+{
+	size_t lecturas;	     //se contabilizan siempre.
+	size_t escrituras;      //lecturas + escrituras = N instrucciones.
+	size_t hits;		   //se dan si coinciden (set, tag y validate bit).
+	size_t clean_miss;    //Se activa cuando dirty bit = 0.
+	size_t dirty_rmiss;  //Se contabiliza cuando rmiss + dirty bit = 1.
+	size_t dirty_wmiss; //Se contabiliza cuando wmiss + dirty bit = 1.
+	size_t rmiss;	   //rmiss + wmiss = total miss.
+	size_t wmiss;
+}estadisticas_t;
+
+
+cache_t* cache_crear(size_t tam, size_t asociatividad, size_t num_sets);
+
+void cache_destruir(cache_t* cache);
+
 /*
-pub struct OpResult {
-    optype: OpType,
-    result: Lookup,  // Hit, clean miss, dirty miss.
-    addr: Addr,     // Unpacked address (index, tag).
-    line_num: usize,
-    valid_bit: u8,
-    dirty_bit: u8,
-    prev_tag: Option<u32>,      // Only present if line was valid.
-    prev_mtime: Option<usize>, // Only present if E > 1.
-}
+*   Dado una matriz cache devuelve el bloque menos usado, siempre devuelve un bloque. 
 */
+bloque_t* encontrar_LRU(cache_t* cache);
 
-typedef enum resultado = {HIT, CLEAN_MISS, DIRTY_MISS} resultado_t;
+op_result_t* cache_operar(cache_t* cache, char* op, size_t dir, size_t tam, size_t datos);
 
-typedef struct addr
-{
-    int tag;
-    int index;
-    int off;
-}addr_t;
-
-typedef struct resultado_op
-{
-    char operacion;
-    resultado_t resultado;
-    addr_t direccion;
-    size_t instruccion;    //indica el numero e linea.
-    bool valid_bit;
-    bool dirty-bit;
-    size_t prev_tag;    //???
-    size_t prev_mtime; //???
-}resultado_op_t;
-
-//Empaqueta los argumentos necesarios para pasar el struct al cache. 
-typedef struct linea
-{
-    size_t instruccion;       //ID instruccion.
-    char operacion;          //Operacion w o r.
-    size_t direccion;       //Direccion de 32 bits en la que se hace el acceso.
-    int bytes_escritos;    //Indica la cantidad de bytes a escribir/leer.
-    size_t datos          //Indica la el dato a operar.
-}linea_t;
-
-
-typedef struct bloque
-{
-	bool es_valido;  //se activa cuando se realiza una escritura por primera vez en ese set/tag.
-	bool dirty_bit; //se activa cuando se produce un miss y se realiza una escritura.
-	size_t tag;    //identifica el bloque.
-	void* data;   //la unidad minima es de 2 bytes, = void dato[tope];
-}bloque_t;
-
-typedef struct set //fila de bloques, la cantidad de bloques queda definida por el archivo
-{
-    size_t E; // Cantidad de bloques = "E"
-	bloque_t* bloques; 	   // = bloque_t bloques[]
-}set_t;
-
-struct cache
-{
-    int time; // Se usa para el last recently used, numero de operacion actual. 
-    size_t S; // Parametro "S" cantidad de sets
-    set_t* sets;
-};
-
-//bytes written = (escrituras + wmiss) * unidad_datos.
-//bytes read = (lecturas + rmiss) * unidad_datos.
-//miss rate = total miss / (lecturas + escrituras).
-//capacidad cache = S x E x Unidad Datos.
 #endif
